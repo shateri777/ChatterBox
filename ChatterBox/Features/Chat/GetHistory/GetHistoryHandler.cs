8f74548f84f74f35d7adecaee88aecb9c0e13bb8
Kamyar Shateri Kashi <kamyarshateri@gmail.com>
2025-09-25 09:06:55 +0200

    created our history handler which return a new hitosry response with messaged that were saved in our db.

---
+++ ChatterBox/Features/Chat/GetHistory/GetHistoryHandler.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Core;
+using Azure;
+using ChatterBox.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatterBox.Features.Chat.GetHistory
+{
+    public class GetHistoryHandler
+    {
+        private readonly ChatterBoxDbContext _db;
+
+        public GetHistoryHandler(ChatterBoxDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task <GetHistoryResponse> Handle(GetHistoryRequest request)
+        {
+            var messages = await _db.ChatMessages
+                .OrderByDescending(m => m.CreatedAt)
+                .Take(request.Limit)
+                .OrderBy(m => m.CreatedAt)
+                .ToListAsync();
+
+            return new GetHistoryResponse { Messages = messages };
+        }
+    }
+}
