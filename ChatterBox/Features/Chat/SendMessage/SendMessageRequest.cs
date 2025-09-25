4d545a8e76603c6b9cbd4905812a26875b803976
Kamyar Shateri Kashi <kamyarshateri@gmail.com>
2025-09-25 06:59:25 +0200

    creating our sendmessage request, with string as message and list of messages.

---
+++ ChatterBox/Features/Chat/SendMessage/SendMessageRequest.cs
@@ -0,0 +1,10 @@
+﻿using ChatterBox.Data.DTOs;
+
+namespace ChatterBox.Features.Chat.SendMessage
+{
+    public class SendMessageRequest
+    {
+        public string Message { get; set; }
+        public List<ChatHistoryItemDTO> History { get; set; }
+    }
+}
