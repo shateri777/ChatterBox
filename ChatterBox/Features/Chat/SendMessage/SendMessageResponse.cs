b64f054f134577c340d9a76de02cd828a36841d6
Kamyar Shateri Kashi <kamyarshateri@gmail.com>
2025-09-25 09:04:57 +0200

    created our resposne for our sendmessage method, which will contatin id, userprompt, airesponse and dateTime

---
+++ ChatterBox/Features/Chat/SendMessage/SendMessageResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatterBox.Features.Chat.SendMessage
+{
+    public class SendMessageResponse
+    {
+        public int Id { get; set; }
+        public string UserPrompt { get; set; }
+        public string AiResponse { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
