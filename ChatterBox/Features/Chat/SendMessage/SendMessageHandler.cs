ef1e90388de416bea09fe4e0356b20828361e70c
Kamyar Shateri Kashi <kamyarshateri@gmail.com>
2025-09-25 09:05:34 +0200

    created our sendmessagehandler, we use our airesponseasync method here and return a response.

---
+++ ChatterBox/Features/Chat/SendMessage/SendMessageHandler.cs
@@ -0,0 +1,37 @@
+﻿using ChatterBox.Data;
+using ChatterBox.Infrastructure.Ai;
+using ChatterBox.Models;
+
+namespace ChatterBox.Features.Chat.SendMessage
+{
+    public class SendMessageHandler
+    {
+        private readonly ChatterBoxDbContext _db;
+        private readonly IAiService _aiService;
+        public SendMessageHandler(ChatterBoxDbContext db, IAiService aiService)
+        {
+            _db = db;
+            _aiService = aiService;
+        }
+        public async Task<SendMessageResponse> Handle(SendMessageRequest request)
+        {
+            var aiReplyText = await _aiService.GetAiResponseAsync(request.Message, request.History);
+            var usermessage = new ChatInteraction
+            {
+                UserPrompt = request.Message,
+                AiResponse = aiReplyText,
+                CreatedAt = DateTime.UtcNow,
+            };
+            await _db.ChatMessages.AddAsync(usermessage);
+            await _db.SaveChangesAsync();
+            var response = new SendMessageResponse
+            {
+                Id = usermessage.Id,
+                UserPrompt = request.Message,
+                AiResponse = aiReplyText,
+                CreatedAt = DateTime.UtcNow,
+            };
+            return response;
+        }
+    }
+}
